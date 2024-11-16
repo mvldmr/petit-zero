e5eb88c8b0a7f9e5b08a54c9fb1f8b58836b8fed
mvldmr <unknwnlifestyle@gmail.com>
2024-11-16 21:58:27 +0100

    chore

--- services/API.js
+++
@@ -1,9 +0,0 @@
-const API = {
-  url: "/data/zero.json",
-  fetchData: async () => {
-    const result = await fetch(API.url);
-    return await result.json();
-  },
-};
-
-export default API;
--- services/GetData.js
+++ services/GetData.js
@@ -1,5 +1,8 @@
-import API from "./API.js";
+async function fetchData(url) {
+  const result = await fetch(url);
+  return await result.json();
+}
 
 export async function loadData() {
-  return await API.fetchData();
+  return await fetchData("/data/zero.json");
 }
